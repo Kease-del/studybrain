ea5152a6c833997ce1896d11b8905248c2b51db3
Kease-del <timilehin4uall@gmail.com>
2026-07-31 23:45:03 +0100

    chore: boot-test step 2 - BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import "@/styles/globals.css"
 
 createRoot(document.getElementById("root")).render(
-  <h1 style={{ color: "white", background: "#111", padding: 40 }}>
-    StudyBrain Boot Test
-  </h1>
+  <BrowserRouter>
+    <h1 style={{ color: "white", background: "#111", padding: 40 }}>
+      StudyBrain Boot Test
+    </h1>
+  </BrowserRouter>
 )
