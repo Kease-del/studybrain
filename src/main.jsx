d5cad640f27a319c34d87cd9784d94175ccb4610
Kease-del <timilehin4uall@gmail.com>
2026-07-31 23:40:09 +0100

    chore: make boot-test text visible on light background

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 
 createRoot(document.getElementById("root")).render(
-  <h1 style={{ color: "white", padding: 40 }}>
+  <h1 style={{ color: "white", background: "#111", padding: 40 }}>
     StudyBrain Boot Test
   </h1>
 )
