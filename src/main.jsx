096965ba6f5d87584aebf076d0fd23340006e6bd
Kease-del <timilehin4uall@gmail.com>
2026-07-31 23:43:43 +0100

    chore: boot-test step 1 - globals.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
+import "@/styles/globals.css"
 
 createRoot(document.getElementById("root")).render(
   <h1 style={{ color: "white", background: "#111", padding: 40 }}>
