2ba92a4cb98abb71ecbeceadd20f08178994fb0a
Kease-del <timilehin4uall@gmail.com>
2026-07-31 23:39:34 +0100

    chore: minimal boot-test for mobile diagnosis (temporary)

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,8 @@
-import { StrictMode } from "react"
+import React from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import { AuthProvider } from "@/context/AuthContext"
-import { QuickCaptureProvider } from "@/context/QuickCaptureContext"
-import { NotesProvider } from "@/context/NotesContext"
-import { VaultProvider } from "@/context/VaultContext"
-import { GoalsProvider } from "@/context/GoalsContext"
-import { SearchProvider } from "@/context/SearchContext"
-import App from "@/App"
-import "@/styles/globals.css"
-import { Toaster } from "react-hot-toast"
-import ErrorBoundary from "@/components/ErrorBoundary"
 
 createRoot(document.getElementById("root")).render(
-  <ErrorBoundary>
-    <StrictMode>
-      <BrowserRouter>
-        <AuthProvider>
-          <NotesProvider>
-            <VaultProvider>
-              <GoalsProvider>
-                <SearchProvider>
-                  <QuickCaptureProvider>
-                    <App />
-                  </QuickCaptureProvider>
-                </SearchProvider>
-              </GoalsProvider>
-            </VaultProvider>
-          </NotesProvider>
-          <Toaster position="top-right" />
-        </AuthProvider>
-      </BrowserRouter>
-    </StrictMode>
-  </ErrorBoundary>
+  <h1 style={{ color: "white", padding: 40 }}>
+    StudyBrain Boot Test
+  </h1>
 )
